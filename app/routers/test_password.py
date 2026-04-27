d896846a1cc156f14116fad132c1421224bb446f
hacker <hacker@DESKTOP-TT1LKGV.Dlink>
2026-04-27 15:12:10 +0300

    wadadawdawd

--- app/routers/test_password.py
+++ app/routers/test_password.py
@@ -1,6 +1,12 @@
 import subprocess
+import os
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def java_chek(password):
-    result = subprocess.run(['java', '-jar', 'PasswordStrength.jar', password], capture_output=True, text=True)
-    return result.stdout.strip()
-
+    result = subprocess.run(
+        ['java', '-cp', CURRENT_DIR, 'Chek', password],
+        capture_output=True,
+        text=True
+    )
+    return result.stdout.strip().lower()
